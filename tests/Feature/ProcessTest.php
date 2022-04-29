5de0945d672230103d983674da0368bf47989af9
Thuku <xthukuh@gmail.com>
2022-04-30 00:40:22 +0300

    latest

--- tests/Feature/ProcessTest.php
+++ tests/Feature/ProcessTest.php
@@ -8,9 +8,7 @@ use xthukuh\Process;
 class ProcessTest extends TestCase
 {
 	/**
-	 * Process example.
-	 * 
-	 * DEBUG: This is still in development
+	 * Process test example.
 	 *
 	 * @return void
 	 */
@@ -18,62 +16,55 @@ class ProcessTest extends TestCase
 		
 		#(bool) check if platform is windows
 		$is_win = Process::is_win($uname);
-		$php = $is_win ? getenv('PHP_CMD') : getenv('PHP_BASH');
+		$php = $is_win ? 'php' : '/usr/local/bin/php';
 		echo '> Test process (my-pid: ' . getmypid() . ", uname=$uname, php=$php)\n";
 		$test_start = microtime(1);
 
-		//DEBUG:
 		#process (test) cmd
-		$loop = 10; //loop count
-		$delay = 1; //seconds
-		$cmd = sprintf('%s sleep.php %d %d', $php, $loop, $delay);
+		$loop = 3; //DEBUG: (int) loop count
+		$delay = 1; //DEBUG: (int) seconds
+		//$cmd = sprintf('%s sleep.php %d %d', $php, $loop, $delay);
+		$cmd = sprintf('%s sleep.php %d %d > sleep.log', $php, $loop, $delay);
 
 		#process options
 		$options=[
-			'cwd' => __DIR__, //set working directory
+			'cwd' => __DIR__, //DEBUG: working directory
 		];
 		
 		#(bool) create process instance
 		$process = new Process($cmd, $options);
 
-		//DEBUG:
 		#(bool) open process
-		$background = 1;
+		$background = 1; //DEBUG: 0, 1
 		$success = $process -> open($background, $callback=function(Process $p){
 			echo "process open callback:\n";
 			print_r([
-				'ccmd' => $p -> ccmd, //(string) process current command line.
-				'cwd' => $p -> cwd, //(string) process working directory.
-				'id' => $p -> id, //(string) process unique id.
-				'background' => json_encode($p -> background), //(bool) process is running in background.
-				'ppid' => $p -> ppid, //(int) process parent pid.
-				'cpid' => $p -> cpid, //(int) process child pid.
-				'pid' => $p -> pid, //(int) process current pid.
+				'id' => $p -> id,
+				'key' => $p -> key,
+				'cwd' => $p -> cwd,
+				'cmd' => $p -> cmd,
+				'ccmd' => $p -> ccmd,
+				'background' => $p -> background,
+				'ppid' => $p -> ppid,
+				'cpid' => $p -> cpid,
+				'pid' => $p -> pid,
 			]);
 			return true; //keep open
 		});
-
-		#check failure
-		if (!$success) die($process -> error);
-		echo '$process -> open(...) = ' . json_encode($success) . "\n";
+		echo '$success = ' . json_encode($success) . "\n";
 
 		#(int|null) get child process pid
-		$child_pid = Process::child($process -> ppid);
-		echo 'Process::child(ppid=' . $process -> ppid . ') = ' . json_encode($child_pid) . "\n";
+		$res = Process::child($process -> ppid, $pids);
+		echo 'Process::child(ppid=' . $process -> ppid . ') = ' . json_encode($res) . " (pids=" . json_encode($pids) . ")\n";
 		
 		#(bool) check if process is running
-		$is_running = $process -> running();
-		echo '$process -> running() = ' . json_encode($is_running) . "\n";
-
-		#check if current process exists
-		$exists = Process::exists($process -> pid);
-		echo 'Process::exists(pid=' . $process -> pid . ') = ' . json_encode($exists) . "\n";
+		$is_running = $process -> running($pid);
+		echo '$process -> running() = ' . json_encode($is_running) . " (pid=$pid)\n";
 		
-		//DEBUG:
 		#process output buffer (print enabled)
-		$get_output = 1;
-		$output_print = 2;
-		$output_timeout = 1;
+		$get_output = 1; //DEBUG: 0, 1
+		$output_print = 2; //DEBUG: 0, 1, 2, 3
+		$output_timeout = 1; //DEBUG: (float) seconds
 		if ($get_output){
 			echo "\n--------- output start --------\n";
 			$output = null;
@@ -100,42 +91,54 @@ class ProcessTest extends TestCase
 		#check if parent process exists
 		$exists = Process::exists($process -> ppid);
 		echo 'Process::exists(ppid=' . $process -> ppid . ') = ' . json_encode($exists) . "\n";
+		
+		#check process open status
 		echo '$process -> open = ' . json_encode($process -> open) . "\n";
 
-		//DEBUG:
 		#(bool, $killed=(null|1|0)) kill process
-		$process_kill = 1;
+		$process_kill = 0; //DEBUG: 0, 1
 		if ($process_kill){
-			$kill = Process::kill($child_pid, $killed);
-			echo 'Process::kill(' . $child_pid . ') = ' . json_encode($kill) . ', $killed=' . json_encode($killed) . "\n";
+			echo "\n- kill\n\n";
+			$kill = Process::kill($process -> pid, $killed);
+			echo 'Process::kill(pid=' . $process -> pid . ') = ' . json_encode($kill) . ' ($killed=' . json_encode($killed) . ")\n";
 		}
 		
 		#(int|null) close process.
-		$exit_code = $process -> close();
-		echo '$process -> close() = ' . json_encode($exit_code) . "\n";
+		echo "\n- close\n\n";
+		$exit = $process -> close();
+		echo '$process -> close() = ' . json_encode($exit) . "\n";
 		echo '$process -> open = ' . json_encode($process -> open) . "\n";
 
-		#delay
-		$d = 2;
-		echo "\n- delay $d second...\n\n";
-		sleep($d);
+		#(bool) check if process is running
+		$is_running = $process -> running($pid);
+		echo '$process -> running() = ' . json_encode($is_running) . " (pid=$pid)\n";
 
 		#check if current process exists
+		$exists = Process::exists($process -> pid);
+		echo 'Process::exists(pid=' . $process -> pid . ') = ' . json_encode($exists) . "\n";
+
+		#check if parent process exists
 		$exists = Process::exists($process -> ppid);
 		echo 'Process::exists(ppid=' . $process -> ppid . ') = ' . json_encode($exists) . "\n";
-		
-		#check if parent process exists
-		$exists = Process::exists($process -> cpid);
-		echo 'Process::exists(cpid=' . $process -> cpid . ') = ' . json_encode($exists) . "\n";
-		if ($exists) echo "\n\n- tasklist | findstr /i " . $process -> cpid . "\n\n";
 
-		#(int|null) process exit return code.
-		$process -> exit;
-		echo '$process -> exit = ' . json_encode($process -> exit) . "\n";
+		#delay
+		$delay = 2; //DEBUG: (float) seconds
+		if ($delay){
+			echo "\n- delay $delay second...\n\n";
+			sleep($delay);
+		}
+
+		#check if current process exists
+		$exists = Process::exists($process -> pid);
+		echo 'Process::exists(pid=' . $process -> pid . ') = ' . json_encode($exists) . "\n";
+		
 		
 		#test done
+		$exit = $process -> exit;
 		$eta = round(microtime(1) - $test_start, 4);
-		echo "\n> Test done ($eta seconds).\n\n";
+		echo "\n> Test done ($eta seconds, exit=$exit).\n";
+
+		#assert
 		$this -> assertTrue(true);
 	}
 }
--- tests/Feature/sleep.php
+++ tests/Feature/sleep.php
@@ -1,13 +1,17 @@
 <?php
 header('content-type: text/plain');
+$pid = getmypid();
+$start = microtime(1);
 $loop = 5;
 $delay = 1;
 $loop = is_array($argv) && isset($argv[1]) && is_numeric($argv[1]) && ($tmp = (int) $argv[1]) > 0 ? $tmp : $loop;
 $delay = is_array($argv) && isset($argv[2]) && is_numeric($argv[2]) && ($tmp = (int) $argv[2]) > 0 ? $tmp : $delay;
-echo "Test sleep (loop=$loop, delay=$delay seconds)\n";
+echo "Test sleep (loop=$loop, delay=$delay seconds, pid=$pid)\n";
 for ($i = 1; $i <= $loop; $i ++){
 	echo "$i - sleep delay...\n";
+	//if ($i > 2) throw new Exception('Test error.');
 	sleep($delay);
 }
-echo "Test complete.\n";
+$eta = round(microtime(1) - $start, 4);
+echo "Test complete ($eta seconds).\n";
 exit(0);
